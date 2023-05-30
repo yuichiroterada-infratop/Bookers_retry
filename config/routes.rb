b7a7e126f6bd89f352ad5aee0ad5bed39915a030
EC2 Default User <ec2-user@ip-172-31-13-223.ap-northeast-1.compute.internal>
2023-05-30 05:39:26 +0000

    [Fin]Topページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  resources :books
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
